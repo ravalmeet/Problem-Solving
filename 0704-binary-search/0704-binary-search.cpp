8e9006170ca81bbb213595c9ef544298d96221eb
Meet Raval <84512175+ravalmeet@users.noreply.github.com>
2023-09-15 14:59:24 +0530

    Time: 32 ms (39.94%), Space: 28 MB (10.78%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    int bs(vector<int>&nums,int s,int e,int target)
+    {
+    if(s>e)
+    {
+        return -1;
+    }
+    int mid=(s+e)/2;
+    
+    if(nums[mid]==target)
+    {
+        return mid;
+    }
+    else if(nums[mid]>target)
+    {
+        return bs(nums,s,mid-1,target);
+    }
+    else
+    { return  bs(nums,mid +1,e,target);}
+
+    }
+    
+    int search(vector<int>& nums, int target) {
+    
+        return bs(nums,0,nums.size()-1,target);
+    
+    }
+};
